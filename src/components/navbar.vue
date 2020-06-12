1e4ccf842a66fd84044c411053f470c5748255c4
Andrea Tampella <48050801+AndreaTUniTn@users.noreply.github.com>
2020-06-12 16:52:32 +0200

    Aggiunta la barra di ricerca e il menu
    Menu laterale okay.
    
    La ricerca non porta alla pagina in specifico corretta

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,49 +1,113 @@
 <template>
     <!-- Creo la navbar che sarà sempre presente e fissa durante la navigazione -->
-    <md-app-toolbar class="md-primary nav">
-        <md-button to="/" class="md-icon-button ">
-            <md-icon>home</md-icon>
-        </md-button>
-        <!-- <md-button to="/1" class="link">1</md-button>
-        <md-button to="/2" class="link ">2</md-button>
-        <md-button to="/3" class="link ">3</md-button>
-        <md-button to="/4" class="link ">4</md-button>
-        <md-button to="/5" class="link  ">5</md-button> 
-        <div class="md-toolbar-section-end">
-            <md-autocomplete class="md-layout-item search" md-layout="box" v-model="SelectedGame" :md-options="searchOptions" @md-changed="search" @md-selected="go">
-                <label class="md-accent">Search...</label>
-            </md-autocomplete>
-        </div> -->
-    </md-app-toolbar>  
+    <div class="toolbar">
+        <md-app-toolbar class="md-primary">
+
+            <!-- SECTION START -->
+            <div class="md-toolbar-section-start">
+
+                <!-- ICONA HAMBURGER -->
+                <md-button class="md-icon-button" @click="showNavigation = true">
+                    <md-icon>menu</md-icon>
+                </md-button>
+
+                <!-- TITOLO -->
+                <span class="md-title">Giochi</span>
+
+            </div>
+
+            <!-- MIDDLE SECTION -->
+            
+            <!-- -------------- -->
+
+
+            <!-- SECTION END -->
+            <div class="md-toolbar-section-end">
+
+                <!-- BARRA DI RICERCA -->
+                <md-autocomplete 
+                class="searchBar" 
+                md-layout="box" 
+                v-model="SelectedGame" 
+                :md-options="searchOptions"
+                @md-changed="search"
+                @md-selected="select"
+                >
+
+                <label >Cerca un gioco</label>
+
+                </md-autocomplete>
+
+            </div>
+
+        </md-app-toolbar>
+
+        <!-- MENU LATERALE A SCOMPARSA -->
+        <md-drawer :md-active.sync="showNavigation" md-swipeable>
+            <!-- TITOLO DEL MENU LATERALE -->
+            <md-toolbar class="md-transparent" md-elevation="0">
+                <span class="md-title">My App name</span>
+            </md-toolbar>
+
+            <!-- LISTA ELEMENTI DEL MENU -->
+            <md-list>
+                <md-list-item>
+                    <md-icon>move_to_inbox</md-icon>
+                    <span class="md-list-item-text">Inbox</span>
+                </md-list-item>
+
+                <md-list-item>
+                    <md-icon>send</md-icon>
+                    <span class="md-list-item-text">Sent Mail</span>
+                </md-list-item>
+
+                <md-list-item>
+                    <md-icon>delete</md-icon>
+                    <span class="md-list-item-text">Trash</span>
+                </md-list-item>
+
+                <md-list-item>
+                    <md-icon>error</md-icon>
+                    <span class="md-list-item-text">Spam</span>
+                </md-list-item>
+            </md-list>
+
+        </md-drawer>
+
+    </div>
 </template>
 
 <script>
-    import dataservice from '@/dataservice.js'
+    import dataservice from '@/dataservice.js';
 
     export default {
-        name: 'Navbar',
+        name: 'App',
         data: function() {
             return {
-                showNav: false,
+                showNavigation: false,
                 SelectedGame: null,
-                searchOptions: [],
-            }
+                searchOptions: []
+            };
         },
-        methods:{
-            // chiamo la ricerca delle corrispondenze
-            search: function(text){
-                this.searchOptions = dataservice.searchGame(text);
+        methods: {
+            toggleMenu () {
+                console.log("Dentro il menu");
+                this.menuVisible = !this.menuVisible
             },
-            // reindirizzo la navigazione quando si seleziona il gioco
-            go: function(found){
-                this.$router.push({ path: '/game/' + found});
+            search: function(term) {
+                console.log("Ciao, sei dentro SEARCH");
+                this.searchOptions = dataservice.searchGioco(term);
+            },
+            select: function(selected) {
+                if (selected === this.$route.params.id) return;
+                this.$router.push({path: '/gioco/' + selected});
             }
-        },
+        }
     }
 </script>
 
 <style scoped>
-.search{
-    max-width: 300px;
+.searchBar {
+  max-width: 100%;
 }
-</style>>
+</style>
